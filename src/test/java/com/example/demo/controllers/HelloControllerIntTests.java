d02b8ca7d7b3b264b8aa15130d4aa81a92082f8a
Eduard Parkhomenko <Eduard.Parkhomenko-CIC@ibm.com>
2022-05-17 21:34:52 +0200

    t

--- src/test/java/com/example/demo/controllers/HelloControllerIntTests.java
+++ src/test/java/com/example/demo/controllers/HelloControllerIntTests.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(HelloController.class)
 class HelloControllerIntTests {
